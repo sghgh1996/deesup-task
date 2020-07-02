03f81885bf71892f1a4a4947a806b2889fb8f41d
sadjad <sghgh1996@yahoo.com>
2020-07-02 23:06:56 +0200

    feat: added sample api call

--- components/news-letter/news-letter-form.vue
+++ components/news-letter/news-letter-form.vue
@@ -4,48 +4,71 @@
       unisciti alla nostra community di <b>design lovers</b>
     </div>
 
-    <ValidationProvider
-      v-slot="validationContext"
-      rules="email|required"
-      class="w-100"
-    >
-      <b-form-input
-        v-model="form.email"
-        type="text"
-        placeholder="Inserisci la tua email"
-        class="mb-2 w-100"
-        aria-describedby="email-live-feedback"
-        :state="getValidationState(validationContext)"
-      />
-      <b-form-invalid-feedback id="email-live-feedback">
-        {{ validationContext.errors[0] }}
-      </b-form-invalid-feedback>
-    </ValidationProvider>
+    <ValidationObserver v-slot="{ handleSubmit }" class="w-100">
+      <b-form @submit.prevent="handleSubmit(subscribe)">
+        <ValidationProvider
+          v-slot="validationContext"
+          rules="email|required"
+          class="w-100"
+        >
+          <b-form-input
+            v-model="form.email"
+            type="text"
+            placeholder="Inserisci la tua email"
+            class="mb-2 w-100"
+            aria-describedby="email-live-feedback"
+            :state="getValidationState(validationContext)"
+          />
+          <b-form-invalid-feedback id="email-live-feedback">
+            {{ validationContext.errors[0] }}
+          </b-form-invalid-feedback>
+        </ValidationProvider>
 
-    <b-button class="button-form my-5 my-lg-4">
-      INVIA
-    </b-button>
+        <b-alert v-if="subscribeStatus === 'error'" variant="danger" dismissible>
+          Dismissible Alert!
+        </b-alert>
 
-    <ValidationProvider
-      v-slot="validationContext"
-      rules="accepted"
-    >
-      <b-form-checkbox
-        v-model="form.privacyStatus"
-        class="d-none d-lg-block mt-4"
-        value="accepted"
-        unchecked-value="not_accepted"
-      >
-        Accetto l'informativa sulla
-        <a href="https://www.iubenda.com/privacy-policy/7828472/legal?ifr=true&height=581" target="blank"> privacy policy</a>
-      </b-form-checkbox>
-      <b-form-invalid-feedback
-        id="privacy-live-feedback"
-        :state="getValidationState(validationContext)"
-      >
-        {{ validationContext.errors[0] }}
-      </b-form-invalid-feedback>
-    </ValidationProvider>
+        <b-overlay
+          :show="subscribeStatus === 'loading'"
+          rounded
+          opacity="0.6"
+          spinner-small
+          spinner-variant="primary"
+          class="my-5 my-lg-4"
+        >
+          <b-button
+            class="button-form w-100"
+            type="submit"
+            :disabled="subscribeStatus === 'loading'"
+          >
+            INVIA
+          </b-button>
+        </b-overlay>
+
+        <ValidationProvider
+          v-slot="validationContext"
+          rules="required"
+          v-if="!isMobileDevice"
+        >
+          <b-form-checkbox
+            v-model="form.privacyStatus"
+            class="d-none d-lg-block mt-4"
+            value="accepted"
+            unchecked-value=""
+            aria-describedby="privacy-live-feedback"
+          >
+            Accetto l'informativa sulla
+            <a href="https://www.iubenda.com/privacy-policy/7828472/legal?ifr=true&height=581" target="blank"> privacy policy</a>
+          </b-form-checkbox>
+          <b-form-invalid-feedback
+            id="privacy-live-feedback"
+            :state="getValidationState(validationContext)"
+          >
+            accetta la privacy
+          </b-form-invalid-feedback>
+        </ValidationProvider>
+      </b-form>
+    </ValidationObserver>
 
     <div class="d-lg-none privacy-text font-weight-bold mt-3 text-justify text-last-center">
       ho letto ed accettato l'informativa sulla
@@ -55,9 +78,10 @@
 </template>
 
 <script>
-import { ValidationProvider } from 'vee-validate'
+import { ValidationProvider, ValidationObserver } from 'vee-validate'
 import { extend } from 'vee-validate'
 import { required, email } from 'vee-validate/dist/rules'
+import { isMobile } from 'mobile-device-detect'
 
 extend('email', {
   ...email,
@@ -69,28 +93,39 @@ extend('required', {
   message: 'Questo campo è obbligatorio'
 })
 
-extend('accepted', {
-  validate: (value) => {
-    return value === 'accepted'
-  },
-  message: 'accetta la privacy'
-})
-
 export default {
   components: {
-    ValidationProvider
+    ValidationProvider,
+    ValidationObserver
   },
   data () {
     return {
       form: {
         email: '',
         privacyStatus: ''
-      }
+      },
+      subscribeStatus: '' // loading - error - sucess
     }
   },
   methods: {
     getValidationState ({ dirty, validated, valid = null }) {
       return dirty || validated ? valid : null
+    },
+    subscribe () {
+      this.subscribeStatus = 'loading'
+      this.$axios.$post('https://reqres.in/api/users', {
+        email: this.form.email
+      }).then((response) => {
+        this.subscribeStatus = 'success'
+        this.$bvModal.hide('news-letter')
+      }).catch(() => {
+        this.subscribeStatus = 'error'
+      })
+    }
+  },
+  computed: {
+    isMobileDevice () {
+      return isMobile
     }
   }
 }
