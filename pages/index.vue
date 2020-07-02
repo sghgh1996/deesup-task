a89fcf09227a59d67614fb4501d933e612a41907
sadjad <sghgh1996@yahoo.com>
2020-07-02 23:22:58 +0200

    fix: some styles

--- pages/index.vue
+++ pages/index.vue
@@ -1,63 +1,18 @@
 <template>
-  <div class="container">
+  <b-container class="container">
     <div>
-      <h1 class="title">
+      <h1>
         deesup-newsletter-popup-task
       </h1>
       <h5>
-        The newsletter popup comes up after 2 seconds from the initial load on every page
+        The newsletter popup comes up after 2 seconds from the initial load
       </h5>
-      <div class="links">
-        <a
-          href="https://github.com/sghgh1996/deesup-task"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          Source Code
-        </a>
-      </div>
+      <b-button
+        href="https://github.com/sghgh1996/deesup-task"
+        variant="success"
+      >
+        Source Code
+      </b-button>
     </div>
-  </div>
+  </b-container>
 </template>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 50px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
